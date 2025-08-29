af90f1d5337e63da9fffe058fa2caadccc37c4fc
Anubhav Mishra <msonu2018@gmail.com>
2025-08-29 11:33:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (86.22%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
@@ -1,23 +1,7 @@
 class Solution {
 public:
     long long flowerGame(int n, int m) {
-        long long oddN = 0, evenN = 0;
-        long long oddM = 0, evenM = 0;
-
-        int i = 1;
-        while (i <= n) {
-            if (i % 2 == 0) evenN++;
-            else oddN++;
-            i++;
-        }
-
-        int j = 1;
-        while (j <= m) {
-            if (j % 2 == 0) evenM++;
-            else oddM++;
-            j++;
-        }
-
-        return oddN * evenM + evenN * oddM;
+        if(n == 1 && m ==1) return 0;
+        return (long long)n*m/2;
     }
 };
